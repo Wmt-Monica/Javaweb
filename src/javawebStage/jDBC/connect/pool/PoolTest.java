6a83374625911ef2bf660bec00973cbd35caa23e
Wmt-Monica <3040988158@qq.com>
2021-01-26 19:36:07 +0800

    2021/1/26

--- src/javawebStage/jDBC/connect/pool/PoolTest.java
+++ src/javawebStage/jDBC/connect/pool/PoolTest.java
@@ -2,6 +2,8 @@ package javawebStage.jDBC.connect.pool;
 
 import javawebStage.jDBC.connect.dao.DAO;
 import org.junit.Test;
+
+import java.sql.Connection;
 import java.sql.SQLException;
 
 public class PoolTest {
@@ -10,8 +12,9 @@ public class PoolTest {
     public void test1() {
         ConnectionPool pool = new ConnectionPool();
         String sql = "insert into animal(animal_id, animal_name) values(1, '石燔猩猩')";
+        Connection conn = pool.getConnection();
         try {
-            new DAO().insert(pool.getConnection(), sql);
+            new DAO().insert(conn, sql);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -22,8 +25,9 @@ public class PoolTest {
         ConnectionPool pool = new ConnectionPool();
         String sql = "insert into animal(animal_id, animal_name) values(?, ?)";
         Object[] objects = {1, "燔猩猩"};
+        Connection conn = pool.getConnection();
         try {
-            new DAO().insert(pool.getConnection(), sql, objects);
+            new DAO().insert(conn, sql, objects);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -33,8 +37,9 @@ public class PoolTest {
     public void test3() {
         ConnectionPool pool = new ConnectionPool();
         String sql = "update animal set animal_id = 2 where animal_name = '燔猩猩'";
+        Connection conn = pool.getConnection();
         try {
-            new DAO().update(pool.getConnection(), sql);
+            new DAO().update(conn, sql);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -45,8 +50,9 @@ public class PoolTest {
         ConnectionPool pool = new ConnectionPool();
         String sql = "update animal set animal_id = ? where animal_name = ?";
         Object[] objects = {2, "燔猩猩"};
+        Connection conn = pool.getConnection();
         try {
-            new DAO().update(pool.getConnection(), sql, objects);
+            new DAO().update(conn, sql, objects);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -56,8 +62,9 @@ public class PoolTest {
     public void test5() {
         ConnectionPool pool = new ConnectionPool();
         String sql = "delete from animal where animal_name = '石燔猩猩'";
+        Connection conn = pool.getConnection();
         try {
-            new DAO().delete(pool.getConnection(), sql);
+            new DAO().delete(conn, sql);
         } catch (SQLException e) {
             e.printStackTrace();
         }
