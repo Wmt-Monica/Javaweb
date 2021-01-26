9cec5675436da59bc77e72522383d7e08a968c1b
Wmt-Monica <3040988158@qq.com>
2021-01-26 22:00:18 +0800

    2021/1/26 创建 C3p0Util 工具类来辅助通过 c3p0 外部导入的连接池来获取 Connection 连接对象的 test 测试

--- src/javawebStage/jDBC/c3p0/C3p0Test.java
+++ src/javawebStage/jDBC/c3p0/C3p0Test.java
@@ -1,6 +1,7 @@
 package javawebStage.jDBC.c3p0;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
+import javawebStage.jDBC.c3p0.util.C3p0Util;
 import org.junit.Test;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -10,6 +11,38 @@ import java.sql.SQLException;
  * 注意：c3p0-config.xml 配置文件需要添加在 src 路径下面，否则创建 Connection 连接对象失败
  */
 public class C3p0Test {
+
+    // 使用 C3p0Util 工具类来获取连接对象
+    @Test
+    public void test2(){
+        Connection conn = null;
+        PreparedStatement pre = null;
+
+        // 创建 SQL 语句
+        String sql = "insert into animal(animal_id, animal_name) values(11, '燔猴子')";
+
+        try {
+            // 根据 c3p0 外来连接池创建连接对象
+            conn = new C3p0Util().getConnection();
+
+            // 根据连接对象创建执行 SQL 语句的 PreparedStatement 对象
+            pre = conn.prepareStatement(sql);
+
+            // 执行 SQL 语句
+            pre.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                pre.close();
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            System.out.println("SQL 语句执行完毕......");
+        }
+    }
+
     @Test
     public void test1(){
         Connection conn = null;
